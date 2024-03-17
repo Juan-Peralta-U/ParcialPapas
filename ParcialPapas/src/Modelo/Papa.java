ae1b7e93746eabbb6d3a98157bfd66629e4d5d0a
AndresRuiz14502 <andresruiz14502@gmail.com>
2024-03-16 20:36:45 -0500

    Formato general y el modelo papas

---
+++ ParcialPapas/src/Modelo/Papa.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelo;
+
+/**
+ *
+ * @author Familia Mora
+ */
+public class Papa {
+
+    private String especie;
+    private String zonaProduccion;
+    private String habtoCrecimiento;
+    private String floracion;
+    private String bayas;
+    private String tuberculos;
+
+    public Papa() {
+    }
+    
+}
